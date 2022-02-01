89aba01869953294d0c7d34897c504b424fff4eb
Angie Pinilla <angie@hashicorp.com>
2022-02-01 12:08:01 -0500

    bucket_notification: add skip to tests that need to migrate to acl resource

--- internal/service/s3/bucket_notification_test.go
+++ internal/service/s3/bucket_notification_test.go
@@ -18,6 +18,9 @@ import (
 )
 
 func TestAccS3BucketNotification_eventbridge(t *testing.T) {
+	// TODO: remove skip once aws_s3_bucket_acl resource is available in the provider
+	t.Skipf("skipping acceptance testing: aws_s3_bucket 'acl' and 'grant' are read-only, migrate configuration to aws_s3_bucket_acl")
+
 	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
 	resourceName := "aws_s3_bucket_notification.notification"
 
